c6aa4b891abeee56ca6b9ae57a5704b24ece8394
caizinn <caiobrasil18@hotmail.com>
2023-05-01 15:21:30 -0300

    Connecting To Database

---
+++ 03_RestWithASP-NET5_ConnectingToDatabase/RestWithASP-NET5/Model/Context/MySQLContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace RestWithASP_NET5.Model.Context
+{
+    public class MySQLContext : DbContext
+    {
+        public MySQLContext()
+        {
+
+        }
+
+        public MySQLContext(DbContextOptions<MySQLContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Person> Persons { get; set; }
+
+    }
+}
---
+++ 03_RestWithASP-NET5_ConnectingToDatabase/RestWithASP-NET5/Services/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using RestWithASP_NET5.Model;
+using RestWithASP_NET5.Controllers;
+
+namespace RestWithASP_NET5.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindByID(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
